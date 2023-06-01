39743b69829ca74b4efdda589abca1286612c975
MishaHryvinskyi <hryvinskyim@gmail.com>
2023-06-01 14:01:33 +0300

    add contact on localStorage

--- src/components/App/App/App.jsx
+++ src/components/App/App/App.jsx
@@ -40,6 +40,19 @@ export class App extends Component {
     );
   };
 
+componentDidMount() {
+  const contacts = localStorage.getItem('contacts');
+  const parsedContacts =JSON.parse(contacts);
+
+  this.setState({ contacts: parsedContacts })
+}
+
+  componentDidUpdate(prevProps, prevState) {
+    if (this.state.contacts !== prevState.contacts) {
+      localStorage.setItem('contacts', JSON.stringify(this.state.contacts));
+    }
+  }
+
   render() {
     const { contacts, filter } = this.state;
     const filteredContacts = this.getFilteredContacts();
